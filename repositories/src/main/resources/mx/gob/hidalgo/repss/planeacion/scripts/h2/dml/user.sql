c5262c1c0992fb0dc88fb4dee7972c2777caca20
kkimvazquezangeles <kkimvazquezangeles@hotmail.com>
2016-04-26 21:56:23 -0500

    modificando sql

--- repositories/src/main/resources/mx/gob/hidalgo/repss/planeacion/scripts/h2/dml/user.sql
+++ repositories/src/main/resources/mx/gob/hidalgo/repss/planeacion/scripts/h2/dml/user.sql
@@ -1,8 +1,26 @@
 INSERT INTO USUARIO (USERNAME, PASSWORD, ENABLED) VALUES
-('admin@hidalgo.gob.mx', 'Adm1n', 1);
+('alberto.xithe@hidalgo.gob.mx', 'Alb3rto1', 1),
+('joaquin.munive@hidalgo.gob.mx', 'Munive37', 1),
+('benjamin.mota@hidalgo.gob.mx', 'Benjam1n', 1),
+('alejandro.vazquez@hidalgo.gob.mx', 'Alevazquez5', 1),
+('luis.arevalo@hidalgo.gob.mx', 'Arevaloluis8', 1),
+('cabaedgu@hotmail.com', 'Edbarron32', 1),
+('mauricio.garcia.ramirez@gmail.com', '54Maugarcia', 1),
+('maribel.oviedo@hidalgo.gob.mx', '3Marioviedo', 1),
+('isaias.parra@hidalgo.gob.mx', '15Parraisais', 1),
+('zaira.martinez.glez@gmail.com', 'Zmartinez23', 1);
 
 
 INSERT INTO USER_ROLE (ID, USERNAME, ROLE) VALUES
-(1, 'admin@hidalgo.gob.mx', 'Admin');
+(1, 'alberto.xithe@hidalgo.gob.mx', 'Director'),
+(2, 'joaquin.munive@hidalgo.gob.mx', 'Director'),
+(3, 'benjamin.mota@hidalgo.gob.mx', 'Director'),
+(4, 'alejandro.vazquez@hidalgo.gob.mx', 'Director'),
+(5, 'luis.arevalo@hidalgo.gob.mx', 'Director'),
+(6, 'cabaedgu@hotmail.com', 'Director'),
+(7, 'mauricio.garcia.ramirez@gmail.com', 'Director'),
+(8, 'maribel.oviedo@hidalgo.gob.mx', 'ADMIN'),
+(9, 'isaias.parra@hidalgo.gob.mx', 'ADMIN'),
+(10, 'zaira.martinez.glez@gmail.com', 'ADMIN');
 
 
